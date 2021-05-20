df4a7cd5ba4359d40beb27d978098626992c0249
Lucio Rendine <rendinel@protonmail.com>
2021-05-20 20:50:48 +0200

    Push

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var app = new Vue({
+el: '#root',
+data: {
+active:true,
+active2:true,
+active3:true,
+active4:true,
+active5:true,
+active6:true,
+},
+method:{
+
+}
+});
+Vue.config.devtools = true;
+
